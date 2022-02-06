165e6565a030a46371c3106da41c4525649e2553
Rudolf <rud.haroutyunyan@gmail.com>
2022-02-06 20:04:41 +0400

    Add project

---
+++ script.js
@@ -0,0 +1,40 @@
+window.onload = function() {
+    window.setInterval(function() {
+        var date = new Date();
+        var hours = date.getHours();
+        var minute = date.getMinutes();
+        var second = date.getSeconds();
+        if (hours < 10) {
+            hours = "0" + hours
+        }
+        if (minute < 10) {
+            minute = "0" + minute
+        }
+        if (second < 10) {
+            second = "0" + second
+        }
+        var clock = hours + (":") + minute + (":") + second
+        document.getElementById("clock").innerHTML = clock;
+
+
+    });
+}
+
+function pas() {
+    var length = 8;
+    var password = ""
+    var string = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[];?><,./-=";
+    for (var i = 0; i < length; i++) {
+        var randomNumber = Math.floor(Math.random() * string.length)
+        password += string.substring(randomNumber, randomNumber + 1)
+
+    }
+    document.getElementById("pass").value = password
+
+}
+
+function myFunction() {
+    var copyText = document.getElementById("pass");
+    copyText.select();
+    document.execCommand("copy");
+}
